1d158af6dc84f6aa116a04b99ae28f093429373a
Elvira <elvira.n.ivanova@gmail.com>
2023-10-16 17:06:20 +0300

    Added components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
+import { useState, useEffect } from "react";
+import Navbar from "./components/Navbar";
+import { ThemeProvider } from "./contex/ThemeContex";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Account from "./pages/Account";
+import axios from "axios";
 
 function App() {
+  const [coins, setCoins] = useState([]);
+
+  const url =
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=10&page=1&sparkline=true&locale=en";
+
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setCoins(response.data);
+      console.log(response.data);
+    });
+  }, [url]);
+
   return (
-    <div >
-      
-    </div>
+    <ThemeProvider>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home coins = {coins} />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/acount " element={<Account />} />
+      </Routes>
+    </ThemeProvider>
   );
 }
 
