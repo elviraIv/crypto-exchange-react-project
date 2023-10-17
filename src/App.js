1a9410edb589a301c67e0836cb7df89909c54d57
Elvira <elvira.n.ivanova@gmail.com>
2023-10-17 16:35:00 +0300

    Added details

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Account from "./pages/Account";
 import axios from "axios";
 import CoinPage from "./pages/CoinPage";
 import Footer from "./components/Footer";
+import { AuthContextProvider } from "./contex/AuthContext";
 
 function App() {
   const [coins, setCoins] = useState([]);
@@ -25,18 +26,19 @@ function App() {
 
   return (
     <ThemeProvider>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home coins={coins} />} />
-        <Route path="/signin" element={<Signin />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/acount " element={<Account />} />
-        <Route path="/coin/:coinId" element={<CoinPage />}>
-          <Route path=":coinId" />
-          <Route />
-        </Route>
-      </Routes>
-      <Footer />
+      <AuthContextProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home coins={coins} />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/account " element={<Account />} />
+          <Route path="/coin/:coinId" element={<CoinPage />}>
+            <Route path=":coinId" />
+          </Route>
+        </Routes>
+        <Footer />
+      </AuthContextProvider>
     </ThemeProvider>
   );
 }
